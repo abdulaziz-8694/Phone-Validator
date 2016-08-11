a076e06cabc82ef57bff93f90e84d09caeac9db9
abdul-8694 <cs12b1001@iith.ac.in>
2016-08-11 18:04:11 +0530

    Initial Commit

---
+++ Phone Validator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Phone Validator
+//
+//  Created by Abdul Aziz on 11/08/16.
+//  Copyright © 2016 Abdul Aziz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
